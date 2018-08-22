5bdf61f9c2035a16bff8a7d1258f1a02d0f557da
Hakim Razalan <hakim.razalan@gmail.com>
2018-08-23 06:06:45 +0800

    change routes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ var router = express.Router();
 var etherScan = require("../controllers/etherScanController.js");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
-});
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 router.post("/get-transactions", etherScan.getTransaction);
 router.post("/add-review", etherScan.addReviews);
