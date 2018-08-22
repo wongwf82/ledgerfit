c66ab0d1e341a5d777130258b2921e4129835037
Hakim Razalan <hakim.razalan@gmail.com>
2018-08-23 06:12:06 +0800

    add seeder & migrate, fix routes

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ var etherScan = require("../controllers/etherScanController.js");
 /* GET home page. */
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  router.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
+  router.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
