d1738699e3f7a090fa4d5b280f8311637f36adf1
Hakim Razalan <hakim.razalan@gmail.com>
2018-08-23 06:19:02 +0800

    fix errors when accessing frontend

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ app.use(function(req, res, next) {
   next();
 });
 
+/* GET home page. */
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 var router = express.Router();
 
 var etherScan = require("../controllers/etherScanController.js");
